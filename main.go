0f864bdc0284061326737e7e5a30966f41faadaf
Wes Hediger <wes.hediger@gmail.com>
2018-05-05 23:04:10 -0500

    add route functions

--- main.go
+++ main.go
@@ -1,17 +1,22 @@
 package main
 
-import {
-    "encoding/json"
-    "log"
-    "net/http"
-    "github.com/gorilla/mux"
-}
+import (
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
 
 func main() {
-    router := mux.NewRouter()
-    router.HandleFunc("/providers", GetProviders).Methods("GET")
-    router.HandleFunc("/providers/{id}", GetProvider).Methods("GET")
-    router.HandleFunc("/providers/{id}", CreateProvider).Methods("GET")
-    router.HandleFunc("/providers/{id}", DeleteProvider).Methods("DELETE")
-    log.Fatal(http.ListenAndServe(":7555", router))
+	router := mux.NewRouter()
+	router.HandleFunc("/providers", GetProviders).Methods("GET")
+	router.HandleFunc("/providers/{id}", GetProvider).Methods("GET")
+	router.HandleFunc("/providers/{id}", CreateProvider).Methods("GET")
+	router.HandleFunc("/providers/{id}", DeleteProvider).Methods("DELETE")
+	log.Fatal(http.ListenAndServe(":7555", router))
 }
+
+func GetProviders(w http.ResponseWriter, r *http.Request) {}
+func GetProvider(w http.ResponseWriter, r *http.Request) {}
+func CreateProvider(w http.ResponseWriter, r *http.Request) {}
+func DeleteProvider(w http.ResponseWriter, r *http.Request) {}
